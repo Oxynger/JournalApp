a81df0bd558f6b386374bd0e7f9b7b159508c8bd
Alex <oxynger@bk.ru>
2019-04-04 23:39:18 +0400

    fix operator api

--- db/model.go
+++ db/model.go
@@ -2,8 +2,6 @@ package db
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Model базовая модел для всех структур который будут записанны в бд.
@@ -13,9 +11,8 @@ import (
 // 	someField some
 // }
 type Model struct {
-	ID        primitive.ObjectID `bson:"_id" json:"ID" example:"5ca10d9d015c736a72b7b3ba"`
-	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 
 	// DeletedAt может быть nil. В таком случае считается что объект не удален
 	DeletedAt *time.Time `bson:"deleted_at" json:"deleted_at"`
