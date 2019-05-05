b519f11fa0333ce4c700dbf1079d91c21796bf88
a00git <a00klr@gmail.com>
2019-05-05 23:14:49 +0400

    wire up all new packages

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
+//go:generate swag init
+
 import (
 	"log"
 
-	"github.com/gin-contrib/cors"
-
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/memstore"
-
-
-	"github.com/Oxynger/JournalApp/api"
-	"github.com/Oxynger/JournalApp/config"
 	"github.com/Oxynger/JournalApp/db"
+	"github.com/Oxynger/JournalApp/router"
 	"github.com/Oxynger/JournalApp/service"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 
@@ -29,10 +28,15 @@ import (
 // @name X-Auth-Token
 
 func init() {
-	conf := config.New()
-	db.Connect(conf.MongoURI)
+	viper.AutomaticEnv()
+	viper.SetDefault("mongodb_uri", "mongodb://localhost:27017")
+	viper.SetDefault("port", "8080")
+	viper.SetDefault("host_domain", "localhost")
 
-	swagdoc.SwaggerInfo.Host = conf.Host
+	db.Connect(viper.GetString("mongodb_uri"))
+
+	swaggerHost := viper.GetString("host") + ":" + viper.GetString("port")
+	swagdoc.SwaggerInfo.Host = swaggerHost
 	swagdoc.SwaggerInfo.BasePath = "/api/v1"
 	swagdoc.SwaggerInfo.Title = "API приложения для составления журналов"
 	swagdoc.SwaggerInfo.Version = "1.1.1"
@@ -40,16 +44,15 @@ func init() {
 }
 
 func main() {
-	router := gin.Default()
-	store := memstore.NewStore([]byte("authtest"))
-	router.Use(cors.Default())
-	router.Use(sessions.Sessions("X-Auth-Token", store))
-	router.Use(api.Auth(service.NewUserService()))
+	app := gin.Default()
+	app.Use(cors.Default())
 
-	api.RegisterV1(router.Group("/api/v1"))
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	users := service.NewUserService()
+	sessions := service.NewSessionService()
+	router.V1(app.Group("/api/v1"), users, sessions)
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	if err := router.Run(); err != nil {
+	if err := app.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
